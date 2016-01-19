485f6980e9d257ead39d8b898e0285b512b0d19f
Paul Water <p.r.water@setbased.nl>
2016-01-19 11:54:41 +0100

    Added unit tests.

--- test/SDoc1Test.py
+++ test/SDoc1Test.py
@@ -1,30 +1,25 @@
+import glob
+import os
+
 from test.SDocTestCase import SDocTestCase
 
 
 class SDoc1Test(SDocTestCase):
     # ------------------------------------------------------------------------------------------------------------------
-    def test1(self):
-        sdoc1 = """
-\expression{a=1}
-\debug{a}
-"""
-        output = self.run_output_test(sdoc1)
-        self.assertEqual(output, "'a' => 1")
+    def testDebug(self):
+        test_file_names = glob.glob(os.path.dirname(os.path.abspath(__file__)) + "/debug/*.sdoc")
 
-    # ------------------------------------------------------------------------------------------------------------------
-    def test2(self):
-        sdoc1 = """
-\expression{a=b=c='abc'}
-\debug{a}
-\debug{b}
-\debug{c}
-"""
-        expected = """'a' => 'abc'
-'b' => 'abc'
-'c' => 'abc'"""
+        for test_file_name in sorted(test_file_names):
+            print(test_file_name)
+            with open(test_file_name, 'r') as file:
+                sdoc1 = file.read()
 
-        output = self.run_output_test(sdoc1)
-        self.assertEqual(output, expected)
+            pre, ext = os.path.splitext(test_file_name)
+            text_file_name = pre + '.txt'
+            with open(text_file_name, 'r') as file:
+                text = file.read()
 
+            output = self.run_output_test(sdoc1)
+            self.assertEqual(output, text)
 
 # ----------------------------------------------------------------------------------------------------------------------
