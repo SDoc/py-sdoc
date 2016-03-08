deb68c2c618fd2913ec3870a5fe8200ead1708f0
Oleg Klimenko <oleg_klimenko@inbox.ru>
2016-03-08 11:48:08 +0200

    Done tables implementation and html formatter for it

---
+++ sdoc/sdoc2/node/TableNode.py
@@ -0,0 +1,143 @@
+"""
+SDoc
+
+Copyright 2016 Set Based IT Consultancy
+
+Licence MIT
+"""
+# ----------------------------------------------------------------------------------------------------------------------
+import re
+import csv
+import io
+from sdoc.sdoc2 import node_store, in_scope, out_scope
+from sdoc.sdoc2.node.Node import Node
+
+
+class TableNode(Node):
+    """
+    SDoc2 node for table.
+    """
+
+    # ------------------------------------------------------------------------------------------------------------------
+    def __init__(self, options):
+        """
+        Object constructor.
+
+        :param dict[str,str] options: The options of this table.
+        """
+        super().__init__('table', options)
+
+    # ------------------------------------------------------------------------------------------------------------------
+    def get_command(self):
+        """
+        Returns the command of this node, i.e. table.
+
+        :rtype: str
+        """
+        return 'table'
+
+    # ------------------------------------------------------------------------------------------------------------------
+    def is_block_command(self):
+        """
+        Returns True.
+
+        :rtype: bool
+        """
+        return True
+
+    # ------------------------------------------------------------------------------------------------------------------
+    def is_inline_command(self):
+        """
+        Returns False.
+
+        :rtype: bool
+        """
+        return False
+
+    # ------------------------------------------------------------------------------------------------------------------
+    def prepare_content_tree(self):
+        """
+        Prepares this node for further processing.
+        """
+        for node_id in self.child_nodes:
+            node = in_scope(node_id)
+
+            self.create_table(node)
+
+            node.prepare_content_tree()
+
+            out_scope(node)
+
+    # ------------------------------------------------------------------------------------------------------------------
+    def create_table(self, node):
+        """
+        Splits text of a TextNode. Creates a table.
+
+        :param sdoc.sdoc2.node.Node.Node node: The node which may be interpreted as table.
+        """
+        temp_table_items = node._argument.split('\n')
+
+        # Remove empty elements.
+        while '' in temp_table_items:
+            temp_table_items.remove('')
+
+        # Create a table.
+        table = []
+        for item in temp_table_items:
+            string = io.StringIO(item)
+            reader = csv.reader(string, delimiter='|')
+            for line in reader:
+                row = line
+                table.append(row)
+
+        if self.is_header_exist(table):
+            self._options['table_header'] = table[0]
+            self._options['table'] = table[2:]
+            self._options['table_aligns'] = self.set_column_alignments(table[1])
+        else:
+            self._options['table'] = table
+
+    # ------------------------------------------------------------------------------------------------------------------
+    @staticmethod
+    def is_header_exist(header_array):
+        """
+        Checks if header is in the table.
+
+        :param list header_array: List with the data.
+
+        :rtype: bool
+        """
+        is_header = True
+        for align in header_array[1]:
+            header_part = re.findall('[:]?[-]+[:]?', align)
+            if not header_part:
+                is_header = False
+                break
+
+        return is_header
+
+    # ------------------------------------------------------------------------------------------------------------------
+    @staticmethod
+    def set_column_alignments(align_array):
+        """
+        Sets alignments on table columns.
+
+        :param list align_array: List of alignments.
+
+        :rtype: list[mixed]
+        """
+        alignments = []
+        for align in align_array:
+            if align.strip().startswith(':') and align.strip().endswith(':'):
+                alignments.append('center')
+            elif align.strip().startswith(':'):
+                alignments.append('left')
+            elif align.strip().endswith(':'):
+                alignments.append('right')
+            else:
+                alignments.append(None)
+
+        return alignments
+
+# ----------------------------------------------------------------------------------------------------------------------
+node_store.register_block_command('table', TableNode)
