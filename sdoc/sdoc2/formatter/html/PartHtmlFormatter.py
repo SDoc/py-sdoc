b297f2e98e5f9b57467530b969abde26f9a6f384
Paul Water <p.r.water@setbased.nl>
2016-07-21 10:07:46 +0200

    Removed very ugly hack.

--- sdoc/sdoc2/formatter/html/PartHtmlFormatter.py
+++ sdoc/sdoc2/formatter/html/PartHtmlFormatter.py
@@ -26,21 +26,8 @@ class PartHtmlFormatter(HtmlFormatter):
         :param sdoc.sdoc2.node.HeadingNode.HeadingNode node: The heading node.
         :param file file: The output file.
         """
-        if os.path.exists('output.html'):
-            os.remove('output.html')
-
-        file_name = 'output_{0}.html'.format(node.argument)
-        self._io.writeln('Writing <fso>{0!s}</fso>'.format(file_name))
-        with open(file_name, 'w') as file:
-            file.write('<!DOCTYPE html><html xmlns="http://www.w3.org/1999/xhtml" xml:lang="NL" lang="NL">')
-            file.write('<head><meta charset="UTF-8"/><title>sdoc</title></head>')
-            file.write('<body>')
-
-            self.generate_part_node(node, file)
-            super().generate(node, file)
-
-            file.write('</body>')
-            file.write('</html>')
+        self.generate_part_node(node, file)
+        super().generate(node, file)
 
     # ------------------------------------------------------------------------------------------------------------------
     @staticmethod
