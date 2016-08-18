793cf764391c7466896e30853a5e49590b7d839f
Paul Water <p.r.water@setbased.nl>
2016-08-18 11:46:54 +0200

    Release: 0.0.10

--- sdoc/application/SdocApplication.py
+++ sdoc/application/SdocApplication.py
@@ -24,7 +24,7 @@ class SDoc(Application):
         """
         Object constructor.
         """
-        Application.__init__(self, 'SDoc', '0.0.9')
+        Application.__init__(self, 'SDoc', '0.0.10')
 
     # ------------------------------------------------------------------------------------------------------------------
     def get_default_commands(self):
