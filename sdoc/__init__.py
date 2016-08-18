580a32cc10f882a34b45457f12eb4929e1a0a87c
Paul Water <p.r.water@setbased.nl>
2016-08-18 11:46:47 +0200

    Fixed __init__.py stuff.

---
+++ sdoc/__init__.py
@@ -0,0 +1,19 @@
+"""
+SDoc
+
+Copyright 2016 Set Based IT Consultancy
+
+Licence MIT
+"""
+# ----------------------------------------------------------------------------------------------------------------------
+from sdoc.application.SdocApplication import SDoc
+
+
+def main():
+    """
+    The main of the sdoc program.
+    """
+    sdoc_application = SDoc()
+    sdoc_application.run()
+
+# ----------------------------------------------------------------------------------------------------------------------
