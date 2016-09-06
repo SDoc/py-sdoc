0d98eadbc974050927f0af80300ed1a125ca359a
Oleg Klimenko <oleg_klimenko@inbox.ru>
2016-09-06 11:00:29 +0300

    SDOC-65: Caption for tables

---
+++ sdoc/sdoc2/formatter/html/CaptionHtmlFormatter.py
@@ -0,0 +1,20 @@
+"""
+SDoc
+
+Copyright 2016 Set Based IT Consultancy
+
+Licence MIT
+"""
+# ---------------------------------------------------------------------------------------------------------------------
+from sdoc.helper.Html import Html
+from sdoc.sdoc2.NodeStore import NodeStore
+from sdoc.sdoc2.formatter.html.HtmlFormatter import HtmlFormatter
+
+
+class CaptionHtmlFormatter(HtmlFormatter):
+    """
+    HtmlFormatter stub generating HTML code for captions.
+    """
+
+# ----------------------------------------------------------------------------------------------------------------------
+NodeStore.register_formatter('caption', 'html', CaptionHtmlFormatter)
