ef0f017e0915c694460ab0a921627302a4e67a30
Paul Water <p.r.water@setbased.nl>
2016-09-15 14:53:05 +0200

    Added document title, date, and version.

--- sdoc/sdoc2/formatter/html/DocumentHtmlFormatter.py
+++ sdoc/sdoc2/formatter/html/DocumentHtmlFormatter.py
@@ -6,6 +6,8 @@ Copyright 2016 Set Based IT Consultancy
 Licence MIT
 """
 # ----------------------------------------------------------------------------------------------------------------------
+from sdoc.helper.Html import Html
+from sdoc.sdoc2 import in_scope, out_scope
 from sdoc.sdoc2.NodeStore import NodeStore
 from sdoc.sdoc2.formatter.html.HtmlFormatter import HtmlFormatter
 
@@ -23,7 +25,41 @@ class DocumentHtmlFormatter(HtmlFormatter):
         :param sdoc.sdoc2.node.DocumentNode.DocumentNode node: The document node.
         :param file file: The output file.
         """
+        self.generate_document_node(node, file)
+
         HtmlFormatter.generate(self, node, file)
+
+    # ------------------------------------------------------------------------------------------------------------------
+    @staticmethod
+    def generate_document_node(node, file):
+        """
+        Generates the HTML code for heading node.
+
+        :param sdoc.sdoc2.node.DocumentNode.DocumentNode node: The document node.
+        :param file file: The output file.
+        """
+        file.write('<div class="sdoc-document-title-outer">')
+        if node.title_node_id:
+            title_node = in_scope(node.title_node_id)
+            file.write(Html.generate_element('h1', {}, title_node.argument))
+            out_scope(title_node)
+
+        file.write('<div class="sdoc-document-title-inner">')
+
+        if node.date_node_id:
+            date_node = in_scope(node.date_node_id)
+            if date_node.argument:
+                file.write(Html.generate_element('span', {'class': 'sdoc-document-date'}, date_node.argument))
+            out_scope(date_node)
+
+        if node.version_node_id:
+            version_node = in_scope(node.version_node_id)
+            if version_node.argument:
+                file.write(Html.generate_element('span', {'class': 'sdoc-document-version'}, version_node.argument))
+            out_scope(version_node)
+
+        file.write('</div>')
+        file.write('</div>')
 
 
 # ----------------------------------------------------------------------------------------------------------------------
