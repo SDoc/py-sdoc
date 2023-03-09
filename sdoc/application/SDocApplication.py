589c2a27eceebb7d96c14744c1632bdbdee9be52
Paul Water <p.r.water@setbased.nl>
2023-03-09 18:44:12 +0100

    Release: 1.0.0.

--- sdoc/application/SDocApplication.py
+++ sdoc/application/SDocApplication.py
@@ -16,7 +16,7 @@ class SDocApplication(Application):
         """
         Object constructor.
         """
-        Application.__init__(self, 'SDocApplication', '0.0.11')
+        Application.__init__(self, 'SDoc', '1.0.0')
 
         self.add(SDocCommand())
         self.add(SDoc1Command())
