f7d35070fb9f1654d2cb104ff09a04d1740c7cdf
Paul Water <p.r.water@setbased.nl>
2023-03-09 18:41:34 +0100

    Release: 1.0.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 setup(
     name='SDoc',
 
-    version='0.9.2',
+    version='1.0.0',
 
     description='A super format documentation document preparation system for SAAS and multi tenant applications',
     long_description=long_description,
